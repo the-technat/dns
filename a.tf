f23c65dc827851fd925bcd02cc7ac393bf5fe1e9
Nathanael <technat@technat.ch>
2022-05-25 10:51:44 +0200

    fix: technat.cloud records

--- a.tf
+++ a.tf
@@ -108,6 +108,15 @@ resource "hetznerdns_record" "cloud_technat_ch_a" {
   ttl     = local.default_ttl
 }
 
+# technat.cloud
+resource "hetznerdns_record" "technat_cloud_a" {
+  zone_id = hetznerdns_zone.technat_cloud.id
+  name    = "@"
+  type    = "A"
+  value   = data.hcloud_server.cloud.ipv4_address
+  ttl     = local.default_ttl
+}
+
 # alleaffengaffen.ch
 resource "hetznerdns_record" "alleaffengaffen_ch_a" {
   zone_id = hetznerdns_zone.alleaffengaffen_ch.id
--- cname.tf
+++ cname.tf
@@ -2,11 +2,3 @@
 # CNAME
 ################
 
-# technat.cloud
-resource "hetznerdns_record" "technat_cloud_cname" {
-  zone_id = hetznerdns_zone.technat_cloud.id
-  name    = "@"
-  type    = "CNAME"
-  value   = "cloud.technat.ch."
-  ttl     = local.default_ttl
-}
