5570550bfd058945a54c906f46ee94c559e74217
Nathanael Liechti <technat@technat.ch>
2022-06-09 05:42:57 -0700

    feat(technat.dev): remove a.technat.dev (#12)

--- ns.tf
+++ ns.tf
@@ -71,36 +71,6 @@ resource "hetznerdns_record" "ns3_technat_dev" {
   ttl     = local.ns_default_ttl
 }
 
-# a.technat.dev
-resource "hetznerdns_record" "ns1_a_technat_dev" {
-  zone_id = hetznerdns_zone.technat_dev.id
-  name    = "a"
-  value   = "ns1-32.azure-dns.com."
-  type    = "NS"
-  ttl     = local.ns_default_ttl
-}
-resource "hetznerdns_record" "ns2_a_technat_dev" {
-  zone_id = hetznerdns_zone.technat_dev.id
-  name    = "a"
-  value   = "ns2-32.azure-dns.net."
-  type    = "NS"
-  ttl     = local.ns_default_ttl
-}
-resource "hetznerdns_record" "ns3_a_technat_dev" {
-  zone_id = hetznerdns_zone.technat_dev.id
-  name    = "a"
-  value   = "ns3-32.azure-dns.org."
-  type    = "NS"
-  ttl     = local.ns_default_ttl
-}
-resource "hetznerdns_record" "ns4_a_technat_dev" {
-  zone_id = hetznerdns_zone.technat_dev.id
-  name    = "a"
-  value   = "ns4-32.azure-dns.info."
-  type    = "NS"
-  ttl     = local.ns_default_ttl
-}
-
 # js-buchsi.ch
 resource "hetznerdns_record" "ns1_js_buchsi_ch" {
   zone_id = hetznerdns_zone.js_buchsi_ch.id
