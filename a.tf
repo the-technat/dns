401370cf6f27ea3516adb4158ea6c6aa2004e5d9
Nathanael Liechti <technat@technat.ch>
2022-06-21 21:37:03 +0200

    feat(preview.js-buchsi.ch): update a records

--- a.tf
+++ a.tf
@@ -19,12 +19,20 @@ resource "hetznerdns_record" "foto_js_buchsi_ch_a" {
   ttl     = local.default_ttl
 }
 
-resource "hetznerdns_record" "preview_js_buchsi_ch_a" {
+resource "hetznerdns_record" "1_preview_js_buchsi_ch_a" {
   zone_id = hetznerdns_zone.js_buchsi_ch.id
   name    = "preview"
   type    = "A"
-  value   = local.webhosting_a
-  ttl     = local.default_ttl
+  value   = "35.172.94.1"
+  ttl     = 1440
+}
+
+resource "hetznerdns_record" "2_preview_js_buchsi_ch_a" {
+  zone_id = hetznerdns_zone.js_buchsi_ch.id
+  name    = "preview"
+  type    = "A"
+  value   = "100.24.208.97"
+  ttl     = 1440
 }
 
 # technat.ch
