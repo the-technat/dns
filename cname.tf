c9cf0fdc62e52c956deae0a1e9b7efe8692d8d6a
Nathanael Liechti <technat@technat.ch>
2022-06-21 21:38:25 +0200

    feat(preview.js-buchsi.ch): add www cname

--- cname.tf
+++ cname.tf
@@ -1,4 +1,10 @@
 ################
 # CNAME
 ################
-
+resource "hetznerdns_record" "www_preview_js_buchsi_ch_a" {
+  zone_id = hetznerdns_zone.js_buchsi_ch.id
+  name    = "www.preview"
+  type    = "CNAME"
+  value   = "s.multiscreensite.com"
+  ttl     = 1440
+}
