fa70383eae393f03a504a07712efcfdd7c4137f1
Nathanael Liechti <technat@technat.ch>
2022-07-02 20:59:04 +0200

    feat(api.technat.ch): update A/AAAA records (#21)

--- a.tf
+++ a.tf
@@ -128,55 +128,7 @@ resource "hetznerdns_record" "api_technat_ch_a" {
   zone_id = hetznerdns_zone.technat_ch.id
   name    = "api"
   type    = "A"
-  value   = "95.217.169.61"
-  ttl     = local.default_ttl
-}
-
-resource "hetznerdns_record" "gorilla_technat_ch_a" {
-  zone_id = hetznerdns_zone.technat_ch.id
-  name    = "gorilla"
-  type    = "A"
-  value   = "65.108.87.190"
-  ttl     = local.default_ttl
-}
-
-resource "hetznerdns_record" "bear_technat_ch_a" {
-  zone_id = hetznerdns_zone.technat_ch.id
-  name    = "bear"
-  type    = "A"
-  value   = "65.21.177.124"
-  ttl     = local.default_ttl
-}
-
-resource "hetznerdns_record" "lion_technat_ch_a" {
-  zone_id = hetznerdns_zone.technat_ch.id
-  name    = "lion"
-  type    = "A"
-  value   = "65.21.176.229"
-  ttl     = local.default_ttl
-}
-
-resource "hetznerdns_record" "mouse_technat_ch_a" {
-  zone_id = hetznerdns_zone.technat_ch.id
-  name    = "mouse"
-  type    = "A"
-  value   = "65.21.178.61"
-  ttl     = local.default_ttl
-}
-
-resource "hetznerdns_record" "lamb_technat_ch_a" {
-  zone_id = hetznerdns_zone.technat_ch.id
-  name    = "lamb"
-  type    = "A"
-  value   = "65.21.178.218"
-  ttl     = local.default_ttl
-}
-
-resource "hetznerdns_record" "duck_technat_ch_a" {
-  zone_id = hetznerdns_zone.technat_ch.id
-  name    = "duck"
-  type    = "A"
-  value   = "65.21.178.29"
+  value   = "95.217.174.160"
   ttl     = local.default_ttl
 }
 
