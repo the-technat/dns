4d39f3028bcbed970db9a6d8018cd92e19414b5e
Nathanael Liechti <technat@technat.ch>
2022-07-10 12:04:14 +0200

    feat: add txt records for mail domaisn

---
+++ txt.tf
@@ -0,0 +1,18 @@
+################
+# TXT Records
+################
+resource "hetznerdns_record" "technat_ch_txt" {
+  zone_id = hetznerdns_zone.technat_ch.id
+  name    = "@"
+  value   = "v=spf1 include:spf.infomaniak.ch ?all"
+  type    = "TXT"
+  ttl     = local.default_ttl
+}
+
+resource "hetznerdns_record" "js_buchsi_ch_txt" {
+  zone_id = hetznerdns_zone.js_buchsi_ch.id
+  name    = "@"
+  value   = "v=spf1 include:spf.infomaniak.ch ?all"
+  type    = "TXT"
+  ttl     = local.default_ttl
+}
